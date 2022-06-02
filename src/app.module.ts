b942158d135481ba53c1580d5f254e5db1423961
shaxboz <shaxboz@abutech.uz>
2022-06-02 22:35:00 +0500

    feat: add get users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { KnexModule } from 'nest-knexjs';
 import { cfg } from '../config/index';
-import { AuthModule } from './modules/users/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-import { AuthController } from './auth/auth.controller';
+import { AuthController } from './modules/auth/auth.controller';
 
 @Module({
   imports: [
