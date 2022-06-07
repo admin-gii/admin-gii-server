eb2a9502e12dda1b03caef19c33f4a24d3631d18
shaxboz <shaxboz@abutech.uz>
2022-06-07 17:25:20 +0500

    feat: add roles CRUD

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { RoleModule } from './modules/roles/role.module';
 import { UsersModule } from './modules/users/users.module';
 
 async function bootstrap() {
@@ -24,7 +25,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config, {
-    include: [UsersModule, AuthModule],
+    include: [UsersModule, AuthModule,RoleModule],
   });
   SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
---
+++ src/modules/roles/role.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { RoleController } from "./role.controller";
+import { RoleService } from "./role.service";
+
+@Module({
+  controllers: [RoleController],
+  providers: [RoleService],
+  exports: [RoleService],
+})
+export class RoleModule {}
