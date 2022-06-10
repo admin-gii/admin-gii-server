70b48f524e9f167f4633a801264e817b5e5647e2
Khurmatillo <72973599+MRXDeveloper23@users.noreply.github.com>
2022-06-10 15:51:02 +0500

    Users CRUD implemented

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ChangeUserDto, CreateUserDto } from './dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -11,5 +21,32 @@ export class UsersController {
   @ApiOkResponse({ description: 'ok' })
   async getUsers(): Promise<any> {
     return await this.usersService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ description: 'ok' })
+  async getUserById(@Param('id', ParseIntPipe) userId: number): Promise<any> {
+    return await this.usersService.getUserById(userId);
+  }
+
+  @Post()
+  @ApiCreatedResponse({ description: 'created' })
+  async createUser(@Body() userDto: CreateUserDto): Promise<any> {
+    return await this.usersService.createUser(userDto);
+  }
+
+  @Put(':id')
+  @ApiOkResponse({ description: 'ok' })
+  async updateUser(
+    @Param('id', ParseIntPipe) userId: number,
+    @Body() userDto: ChangeUserDto,
+  ): Promise<any> {
+    return await this.usersService.updateUser(userId, userDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ description: 'ok' })
+  async deletedUser(@Param('id', ParseIntPipe) userId: number): Promise<any> {
+    return await this.usersService.deleteUser(userId);
   }
 }
