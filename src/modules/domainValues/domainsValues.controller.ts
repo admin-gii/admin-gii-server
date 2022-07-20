f840380fd615d2e2da35c78ba5d5cacb225145e8
shaxboz <shaxboz@abutech.uz>
2022-07-20 11:47:08 +0500

    fix: code review

--- src/modules/domainValues/domainsValues.controller.ts
+++ src/modules/domainValues/domainsValues.controller.ts
@@ -1,10 +1,16 @@
-/*
-https://docs.nestjs.com/controllers#controllers
-*/
-
 import { CreateResponse } from '@/helpers/create.response';
-import { formatArrayObject , formatObject} from '@/helpers/format.object';
-import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { DomainsValuesService } from './domainsValues.service';
@@ -15,45 +21,57 @@ import { CreateDomainValuesDto } from './dto/domainValues.dto';
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 export class DomainsValuesController {
-    constructor(private readonly domainsValuesService: DomainsValuesService) { }
+  constructor(private readonly domainsValuesService: DomainsValuesService) {}
 
-    @Get('domain-values')
-    @HttpCode(200)
-    @ApiOkResponse({ description: 'ok' })
-    async getDomainValues(): Promise<any> {
-       let domainValues =  await this.domainsValuesService.getDomainValues()
-       return CreateResponse(domainValues , 'Get domain-values success')
-    }
+  @Get('domain-values')
+  @HttpCode(200)
+  @ApiOkResponse({ description: 'ok' })
+  async getDomainValues(): Promise<any> {
+    const domainValues = await this.domainsValuesService.getDomainValues();
+    return CreateResponse(domainValues, 'Get domain-values success');
+  }
 
-    @Get('domain-values/:id')
-    @HttpCode(200)
-    @ApiOkResponse({ description: 'ok' })
-    async getDomainValuesById(@Param('id', ParseIntPipe) id: number): Promise<any> {
-        let domainValues  = await this.domainsValuesService.getDomainValuesById(id)
-        return CreateResponse(domainValues , 'Get domain-values by id success')
-    }
+  @Get('domain-values/:id')
+  @HttpCode(200)
+  @ApiOkResponse({ description: 'ok' })
+  async getDomainValuesById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<any> {
+    const domainValues = await this.domainsValuesService.getDomainValuesById(
+      id,
+    );
+    return CreateResponse(domainValues, 'Get domain-values by id success');
+  }
 
-    @Post('domain-values')
-    @HttpCode(200)
-    @ApiOkResponse({ description: 'ok' })
-    async postDomainValues(@Body() dto: CreateDomainValuesDto): Promise<any> {
-        const domainValues = await this.domainsValuesService.postDomainValues(dto)
-        return CreateResponse(domainValues, 'Create domain-values success')
-    }
+  @Post('domain-values')
+  @HttpCode(200)
+  @ApiOkResponse({ description: 'ok' })
+  async postDomainValues(@Body() dto: CreateDomainValuesDto): Promise<any> {
+    const domainValues = await this.domainsValuesService.postDomainValues(dto);
+    return CreateResponse(domainValues, 'Create domain-values success');
+  }
 
-    @Put('domain-values/:id')
-    @HttpCode(200)
-    @ApiOkResponse({ description: 'ok' })
-    async putDomainValues(@Param('id', ParseIntPipe) domainValuesId: number ,@Body() dto: CreateDomainValuesDto): Promise<any> {
-        const domainValues = await this.domainsValuesService.putDomainValues(dto , domainValuesId)
-        return CreateResponse(domainValues, 'Update domain-values success')
-    }
+  @Put('domain-values/:id')
+  @HttpCode(200)
+  @ApiOkResponse({ description: 'ok' })
+  async putDomainValues(
+    @Param('id', ParseIntPipe) domainValuesId: number,
+    @Body() dto: CreateDomainValuesDto,
+  ): Promise<any> {
+    const domainValues = await this.domainsValuesService.putDomainValues(
+      dto,
+      domainValuesId,
+    );
+    return CreateResponse(domainValues, 'Update domain-values success');
+  }
 
-    @Delete('domain-values/:id')
-    @HttpCode(200)
-    @ApiOkResponse({description:'ok'})
-    async deleteDomainValues(@Param('id',ParseIntPipe) domainValuesId:number){
-        const domainValues = await this.domainsValuesService.deleteDomainValues(domainValuesId)
-        return CreateResponse(domainValues, 'Delete domain-values success')
-    }
+  @Delete('domain-values/:id')
+  @HttpCode(200)
+  @ApiOkResponse({ description: 'ok' })
+  async deleteDomainValues(@Param('id', ParseIntPipe) domainValuesId: number) {
+    const domainValues = await this.domainsValuesService.deleteDomainValues(
+      domainValuesId,
+    );
+    return CreateResponse(domainValues, 'Delete domain-values success');
+  }
 }
--- src/modules/domainValues/domainsValues.service.ts
+++ src/modules/domainValues/domainsValues.service.ts
@@ -2,7 +2,7 @@
 https://docs.nestjs.com/providers#services
 */
 
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Knex } from 'knex';
 import { InjectConnection } from 'nest-knexjs';
 import { CreateDomainValuesDto } from './dto/domainValues.dto';
@@ -10,55 +10,77 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class DomainsValuesService {
-    constructor(@InjectConnection() private readonly knex: Knex) { }
+  constructor(@InjectConnection() private readonly knex: Knex) {}
 
-    async getDomainValues() {
-        let domain_values = await this.knex('domain_values as dv')
-            .select(this.knex.raw(`dv.id , dv.value , json_build_object('id',d.id ,'name',d.slug ,'slug',d.slug) as domain, json_build_object('name',df.name ,'slug',df.slug , 'domain' , (select json_build_object('id',d2.id ,'name',d2.slug ,'slug',d2.slug) from domains d2 where d2.id = df.domain_id)) as domainField`)
-            ).innerJoin('domains as d', 'd.id', 'dv.domain_id').innerJoin('domain_fields as df', 'df.id', 'dv.domain_field_id')
-        return domain_values
-    }
+  async getDomainValues() {
+    const domain_values = await this.knex('domain_values as dv')
+      .select(
+        this.knex.raw(
+          `dv.id , dv.value , json_build_object('id',d.id ,'name',d.slug ,'slug',d.slug) as domain, json_build_object('name',df.name ,'slug',df.slug , 'domain' , (select json_build_object('id',d2.id ,'name',d2.slug ,'slug',d2.slug) from domains d2 where d2.id = df.domain_id)) as domainField`,
+        ),
+      )
+      .innerJoin('domains as d', 'd.id', 'dv.domain_id')
+      .innerJoin('domain_fields as df', 'df.id', 'dv.domain_field_id');
+    return domain_values;
+  }
 
-    async getDomainValuesById(id: number) {
-        let domain_values = await this.knex('domain_values as dv')
-            .select(this.knex.raw(`dv.id , dv.value , json_build_object('id',d.id ,'name',d.slug ,'slug',d.slug) as domain, json_build_object('name',df.name ,'slug',df.slug , 'domain' , (select json_build_object('id',d2.id ,'name',d2.slug ,'slug',d2.slug) from domains d2 where d2.id = df.domain_id)) as domainField`)
-            ).innerJoin('domains as d', 'd.id', 'dv.domain_id').innerJoin('domain_fields as df', 'df.id', 'dv.domain_field_id').where({ 'dv.id': id }).first()
-        if (!domain_values) {
-            throw new NotFoundException('domain-values invalid!')
-        }
-        return domain_values
+  async getDomainValuesById(id: number) {
+    const domain_values = await this.knex('domain_values as dv')
+      .select(
+        this.knex.raw(
+          `dv.id , dv.value , json_build_object('id',d.id ,'name',d.slug ,'slug',d.slug) as domain, json_build_object('name',df.name ,'slug',df.slug , 'domain' , (select json_build_object('id',d2.id ,'name',d2.slug ,'slug',d2.slug) from domains d2 where d2.id = df.domain_id)) as domainField`,
+        ),
+      )
+      .innerJoin('domains as d', 'd.id', 'dv.domain_id')
+      .innerJoin('domain_fields as df', 'df.id', 'dv.domain_field_id')
+      .where({ 'dv.id': id })
+      .first();
+    if (!domain_values) {
+      throw new NotFoundException('domain-values invalid!');
     }
+    return domain_values;
+  }
 
-    async postDomainValues(dto: CreateDomainValuesDto) {
-        await this.validateDomainValues(dto)
-        const domain_values = await this.knex('domain_values').insert({ ...dto, group_id: uuidv4() }).returning('id')
-        return domain_values[0]
-    }
+  async postDomainValues(dto: CreateDomainValuesDto) {
+    await this.validateDomainValues(dto);
+    const domain_values = await this.knex('domain_values')
+      .insert({ ...dto, group_id: uuidv4() })
+      .returning('id');
+    return domain_values[0];
+  }
 
-    async putDomainValues(dto: CreateDomainValuesDto, domainValuesId: number) {
-        await this.validateDomainValues(dto)
-        const domain_values = await this.knex('domain_values').update(dto).where({ id: domainValuesId }).returning('id')
-        if (!domain_values.length) {
-            throw new NotFoundException('domain-values invalid!')
-        }
-        return domain_values[0]
+  async putDomainValues(dto: CreateDomainValuesDto, domainValuesId: number) {
+    await this.validateDomainValues(dto);
+    const domain_values = await this.knex('domain_values')
+      .update(dto)
+      .where({ id: domainValuesId })
+      .returning('id');
+    if (!domain_values.length) {
+      throw new NotFoundException('domain-values invalid!');
     }
+    return domain_values[0];
+  }
 
-    async deleteDomainValues(domainValuesId: number) {
-        const domain_values = await this.knex('domain_values').where({ id: domainValuesId }).del().returning('id')
-        if (!domain_values.length) {
-            throw new NotFoundException('domain-values invalid!')
-        }
-        return domain_values[0]
+  async deleteDomainValues(domainValuesId: number) {
+    const domain_values = await this.knex('domain_values')
+      .where({ id: domainValuesId })
+      .del()
+      .returning('id');
+    if (!domain_values.length) {
+      throw new NotFoundException('domain-values invalid!');
     }
+    return domain_values[0];
+  }
 
-    private async validateDomainValues(dto: CreateDomainValuesDto) {
-        const { domain_field_id, domain_id } = dto
-        const domain_fields = await this.knex('domain_fields').where({ id: domain_field_id }).first()
-        const domains = await this.knex('domains').where({ id: domain_id }).first()
-        if (!domains || !domain_fields) {
-            throw new NotFoundException('Domain or Domain field invalid')
-        }
-        return true
+  private async validateDomainValues(dto: CreateDomainValuesDto) {
+    const { domain_field_id, domain_id } = dto;
+    const domain_fields = await this.knex('domain_fields')
+      .where({ id: domain_field_id })
+      .first();
+    const domains = await this.knex('domains').where({ id: domain_id }).first();
+    if (!domains || !domain_fields) {
+      throw new NotFoundException('Domain or Domain field invalid');
     }
+    return true;
+  }
 }
